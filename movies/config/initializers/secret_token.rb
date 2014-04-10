d31158731971482171d3692ff9b729463174da81
DenysDenylson <DenysDenylson@gmail.com>
2014-04-10 08:19:45 -0400

    esto es purbra

---
+++ movies/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Movies::Application.config.secret_key_base = '387109ba35d42ae8ed608f189b87fee7e23a4efb051fbd973e3aace40b7751c6aa3fcfabdfa1d72c9551e16662064fb2ce5c309070a4d20fec8bd2540a7a5c3d'
